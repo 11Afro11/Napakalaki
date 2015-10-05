cb8b9c482387d9ea1b3a89aad0d5d2ba6cf401cf
11Afro11 <javier.fuentes78@gmail.com>
2015-10-05 18:33:37 +0200

    Implementados clases Prize BadConsequence y enum

---
+++ Napakalaki/src/napakalaki/BadConsequence.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+import java.util.ArrayList;
+/**
+ *
+ * @author afro
+ */
+public class BadConsequence {
+    private String text;
+    private int levels;
+    private int nVisibleTreasures;
+    private int nHiddenTeasures;
+    private boolean death;
+    
+    private ArrayList<TreasureKind>specificHiddenTreasure = new ArrayList();
+    private ArrayList<TreasureKind>specificVisibleTreasure = new ArrayList();
+    
+    BadConsequence(String text, int levels, int nVisible, int nHidden){
+        this.text = text;
+        this.levels = levels;
+        nVisibleTreasures = nVisible;
+        nHiddenTeasures = nHidden;
+    }
+    
+    BadConsequence(String text, boolean death){
+        this.text = text;
+        this.death = death;
+    }
+    
+    BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible, ArrayList<TreasureKind> tHidden){
+        this.text = text;
+        this.levels = levels;
+        specificVisibleTreasure = tVisible;
+        specificHiddenTreasure = tHidden;
+    }
+    
+    String getText(){
+        return text;
+    }
+    
+    int getLevels(){
+        return levels;
+    }
+    
+    int getVisibleTreasures(){
+        return nVisibleTreasures;
+    }
+    
+    int getHiddenTreasures(){
+        return nHiddenTeasures;
+    }
+    
+    boolean getDeath(){
+        return death;
+    }
+    
+    
+}
---
+++ Napakalaki/src/napakalaki/Prize.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author afro
+ */
+public class Prize {
+    private int treasures;
+    private int levels;
+    
+    Prize(int treasures, int levels){
+        this.treasures = treasures;
+        this.levels = levels;
+    }
+    
+    int getTreasures(){
+        return treasures;
+    }
+    
+    int getLevels(){
+        return levels;
+    }
+    
+    
+}
