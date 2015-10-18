b9b814bfbb6b3e03c6e1dca3fcb2bcf3947e4a5e
11Afro11 <javier.fuentes78@gmail.com>
2015-10-18 13:06:39 +0200

    Creado chibichulo y bonanza

--- Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package napakalaki;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  *
  * @author afro
@@ -15,11 +16,20 @@ public class PruebaNapakalaki {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String nombre = "Hydra";
-        int combat = 7;
-        Monster premio= new Monster(nombre, combat);
+        ArrayList<Monster>monstruos = new ArrayList();
+        //bonanza
+        BadConsequence badConsequence = new BadConsequence("Pierdes tu armadura visible y otra oculta", 0, new ArrayList(Arrays.asList(TreasureKind.ARMOR)), new ArrayList(Arrays.asList(TreasureKind.ARMOR)));
+        Prize prize = new Prize(2, 1);
         
-        System.out.println("Su monstruo es "+ premio.getName());
+        monstruos.add(new Monster("Byakhees de bonanza", 8, prize, badConsequence));
+        //Chibichulo
+        badConsequence = new BadConsequence("Embobados con ellindo primigenio te descartas tu casco visible", 0, new ArrayList(Arrays.asList(TreasureKind.HELMET)), new ArrayList(Arrays.asList()));
+        prize = new Prize(1, 1);
+        
+        monstruos.add(new Monster("Chibithulhu", 2, prize, badConsequence));
+        
+        
+        
     }
     
 }
