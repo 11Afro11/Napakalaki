dec8be44014403a606676128e644cfab5ba1f20d
David <dividfernandez@correo.ugr.es>
2015-10-18 21:19:06 +0200

    He añadido los monstruos que faltaban

--- Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -67,6 +67,51 @@ public class PruebaNapakalaki {
         prize = new Prize(1, 1);
         monstruos.add(new Monster("Bicéfalo", 20, prize, badConsequence));
         
+        //Los Hondos
+        badConsequence = new BadConsequence("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estas muerto", true);
+        prize = new Prize(2, 1);
+        monstruos.add(new Monster("Los hondos", 8, prize, badConsequence));
+        
+        //Semillas Cthulhu
+        badConsequence = new BadConsequence("Pierdes 2 niveles y 2 tesoros ocultos", 2, 0, 2);
+        prize = new Prize(2, 1);
+        monstruos.add(new Monster("Semillas Cthulhu", 4, prize, badConsequence));
+        
+        //Dameargo
+        badConsequence = new BadConsequence("Te intentas escaquear. Pierdes una mano invisible", 0, 1, 0);
+        prize = new Prize(2, 1);
+        monstruos.add(new Monster("Dameargo", 1, prize, badConsequence));
+        
+        //Pollipolipo Volante
+        badConsequence = new BadConsequence("Da mucho asquito. Pierdes 3 niveles", 3, 0, 0);
+        prize = new Prize(1, 1);
+        monstruos.add(new Monster("Pollipolipo volante", 3, prize, badConsequence));
+        
+        //Yskhtihussg-Goth
+        badConsequence = new BadConsequence("No le hace gracia que pronuncien mal su nombre. Estas muerto", true);
+        prize = new Prize(3, 1);
+        monstruos.add(new Monster("Yskhtihussg-Goth", 12, prize, badConsequence));
+        
+        //Familia Feliz
+        badConsequence = new BadConsequence("La familia te atrapa. Estas muerto", true);
+        prize = new Prize(4, 1);
+        monstruos.add(new Monster("Familia feliz", 1, prize, badConsequence));
+        
+        //Roboggoth
+        badConsequence = new BadConsequence("La quinta directiva primaria te obliga a perder 2 niveles y un tesoro 2 manos visible", 2, 1, 0);
+        prize = new Prize(2, 1);
+        monstruos.add(new Monster("Roboggoth", 8, prize, badConsequence));
+        
+        //El Espia
+        badConsequence = new BadConsequence("Te asusta en la noche. Pierdes un casco visible", 0, new ArrayList(Arrays.asList(TreasureKind.HELMET)), new ArrayList(Arrays.asList(null)));
+        prize = new Prize(1, 1);
+        monstruos.add(new Monster("El espia", 5, prize, badConsequence));
+        
+        //El Lenguas
+        badConsequence = new BadConsequence("Menudo susto te llevas. Pierdes 2 niveles y 5 tesoros visibles", 2, 5, 0);
+        prize = new Prize(1, 1);
+        monstruos.add(new Monster("El Lenguas", 20, prize, badConsequence));
+        
         
         
         
