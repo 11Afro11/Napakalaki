e7ea27d8d3d39f2ec84318a73da03f3f2db143e9
David <dividfernandez@correo.ugr.es>
2015-11-14 00:06:44 +0100

    Añado clases Napakalaki, CardDealer y CombatResult

---
+++ NapakalakiRuby/lib/card_dealer.rb
@@ -0,0 +1,51 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+module Napakalaki
+	require "singleton"
+
+	class CardDealer
+		include Singleton
+
+		def initTreasureCardDeck
+
+		end
+
+		def initMonsterCardDeck
+
+		end
+
+		def shuffleTreasures
+
+		end
+
+		def shuffleMonster
+
+		end
+
+		def getInstance
+			@instance
+		end
+
+		def nextTreasure
+
+		end
+
+		def nextMonster
+
+		end
+
+		def giveTreasureBack(treasure)
+
+		end
+
+		def giveMonsterBack(monster)
+
+		end
+
+		def initCards
+			
+		end
+  end
+end
---
+++ NapakalakiRuby/lib/napakalaki.rb
@@ -0,0 +1,66 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+require_relative "cardDealer"
+require_relative "player"
+
+module Napakalaki
+	include Singleton
+
+	def initPlayers(names)
+
+	end
+
+	def nextPlayer
+		
+	end
+
+	def nextTurnAllowed
+		
+	end
+
+	def setEnemies
+		
+	end
+
+	def getInstance
+		
+	end
+
+	def developCombat
+
+	end
+
+	def discardVisibleTreasures
+		
+	end
+
+	def discardHiddenTreasures
+		
+	end
+
+	def makeTreasuresVisible(treasure)
+		
+	end
+
+	def initGame(players)
+		
+	end
+
+	def getCurrentPlayer
+		
+	end
+
+	def getCurrentMonster
+		
+	end
+
+	def nextTurn
+		
+	end
+
+	def endOfGame
+		
+	end
+end
