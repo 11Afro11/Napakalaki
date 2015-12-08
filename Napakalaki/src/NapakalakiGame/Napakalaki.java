128a2aaccb395a1b33f834e8b14d1182f226f034
11Afro11 <javier.fuentes78@gmail.com>
2015-12-08 15:23:48 +0100

    ejemplo

--- Napakalaki/src/NapakalakiGame/Napakalaki.java
+++ Napakalaki/src/NapakalakiGame/Napakalaki.java
@@ -104,7 +104,7 @@ public class Napakalaki{
 	}
 
     public void initGame(ArrayList<String> names) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
 }
