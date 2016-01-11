eeeab884c9aca796c80ffe2e8c7a56d1a4a522f4
11Afro11 <javier.fuentes78@gmail.com>
2016-01-11 15:52:28 +0100

    b,hjsdv

--- NapakalakiRuby/lib/Napakalaki.rb
+++ NapakalakiRuby/lib/Napakalaki.rb
@@ -4,6 +4,7 @@
 
 require_relative "CardDealer.rb"
 require_relative "Player.rb"
+require_relative "numeric_bad_consequence.rb"
 
 
 require "singleton"
@@ -21,7 +22,7 @@ class Napakalaki
       @carddealer = CardDealer.instance
       @firstPlayer = true
       @currentPlayer = nil
-      @currentMonster = Monster.new("", 0, Prize.new(0, 0), NumericBadConsequence.new("", 0, 0, 0), 0)
+      @currentMonster = Monster.new("", 0, Prize.new(0, 0),NumericBadConsequence("", 0, 0, 0), 0)
       @currentPlayerIndex = -1
     end
    
