f079e3aa5a3272aead0902f3487f1396a2f4ae87
David <dividfernandez@correo.ugr.es>
2016-02-09 10:34:24 +0100

    fdsd

--- Napakalaki/src/GUI/BadConsequenceView.java
+++ Napakalaki/src/GUI/BadConsequenceView.java
@@ -30,10 +30,10 @@ public class BadConsequenceView extends javax.swing.JPanel {
         nVisibleTreasures.setText(Integer.toString(this.badConsequenceModel.getVisibleTreasures()));
         nHiddenTreasures.setText(Integer.toString(this.badConsequenceModel.getHiddenTreasures()));
         if(this.badConsequenceModel.getDeath()){
-            dead.setText("Yes");
+            death.setText("Yes");
         }
         else{
-            dead.setText("No");
+            death.setText("No");
         }
         
     }
@@ -47,19 +47,38 @@ public class BadConsequenceView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel2.setText("Niveles: ");
+
+        jLabel1.setText("Nombre:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addContainerGap(308, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addContainerGap(216, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
